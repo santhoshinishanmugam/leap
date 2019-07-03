17db006c5324e72cd8ce0cb18ce25e3ef8947328
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-03 14:11:23 +0530

    Update set6.py

--- set6.py
+++ set6.py
@@ -2,9 +2,9 @@ year = int(input)
 if year % 4 == 0:
   if year % 100 == 0:
     if year % 400 == 0:
-    print("yes")
+      print("yes")
     else:
-    print("no")
+      print("no")
   else:
     print("yes")
 else:
