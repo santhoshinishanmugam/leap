095226d2ef1858661204a047ba026428cd7bbae1
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-08 17:31:32 +0530

    Update sethun3.py

--- sethun3.py
+++ sethun3.py
@@ -1,1 +1,11 @@
-
+s = int(input())
+n = list(map(int,input().split()))
+temp=[]
+for i in range(s):
+  if n[i] == i:
+    temp.append(n[i])
+if len(temp) == 0:
+  print(-1)
+else:
+  for i in sorted(temp):
+    print(i,end=' ')
