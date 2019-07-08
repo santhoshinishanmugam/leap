ed75b4bb824549c89e40fa1cd816032b5c084c57
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-08 18:21:50 +0530

    Update sethunt5.py

--- sethunt5.py
+++ sethunt5.py
@@ -1,1 +1,13 @@
+s = int(inpuut())
+k=1
+for j in range(len(s)-1):
+  b=s[j]+s[j+1]
+  p=int(b)
+  if p<=26 and s[j]!="0":
+    k+=1
+if k==3:
+  print(k)
+else:
+  print(k+(k-1)//2)
 
+               
